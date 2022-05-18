fccc72ff72e04b31d22ccbd99d7dec19596ee3b1
kostya32223 <kostya32rus94@yandex.ru>
2022-05-18 11:34:17 +0300

    добавлены функции

--- sandbox/src/main/java/ru/stqa/parttwo/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/parttwo/sandbox/MyFirstProgram.java
@@ -4,6 +4,26 @@ public class MyFirstProgram {
 	
 	public static void main(String[] args){
 		
-System.out.println("Hi,world");
+	hello("world");
+	hello("User");
+	hello("Kostya");
+	//первая функция
+	double l = 4;
+	System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+	//вторая функция
+	double a = 3;
+	double b = 5;
+	System.out.println("Площадь прямоугольника со сторонами " + a + " и  " + b +" = " + area(a,b));
+
+	}
+	public static void hello(String somebody){
+		System.out.println("Hi," + somebody);
+
+	}
+	public static double area(double len){
+		return len*len;
+	}
+	public static double area(double a,double b){
+		return a * b;
 	}
 }
